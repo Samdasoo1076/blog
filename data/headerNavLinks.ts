9080c222b8a76c6771f7dbc2bfc598d707d571ee
이지민 <dlwlalsdkd@gmail.com>
2024-05-23 22:25:55 +0900

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: '블로그' },
+  { href: '/tags', title: '태그' },
+  { href: '/projects', title: '프로젝트' },
+  { href: '/about', title: '기타' },
 ]
 
 export default headerNavLinks
