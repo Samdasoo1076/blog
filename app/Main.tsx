d5c1db19fa4b3213b7f2103c7fcf0d969ca96076
이지민 <dlwlalsdkd@gmail.com>
2024-05-31 11:08:46 +0900

    Main 레이아웃, 블로그 글 추가

--- app/Main.tsx
+++ app/Main.tsx
@@ -59,7 +59,7 @@ export default function Home({ posts }) {
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                           aria-label={`Read more: "${title}"`}
                         >
-                          Read more &rarr;
+                          자세히 보기 &rarr;
                         </Link>
                       </div>
                     </div>
