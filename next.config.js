c131a4068a592adb9ce41d8ebb78569dc48cd691
이지민 <dlwlalsdkd@gmail.com>
2024-06-11 16:30:04 +0900

    [fix] RollBack next.config.js

--- next.config.js
+++ next.config.js
@@ -13,8 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app;
-  frame-ancestors 'self' https://giscus.app;
+  frame-src giscus.app
 `
 //frame-ancestors 'self' https://giscus.app이 추가됨
 
